fe80e4c28957cbe7a399ecb9a7bf25fe087e59d1
Aschen <adrien@maret.email>
2022-01-22 21:53:33 +0100

    add js files

---
+++ lib/engine/EngineContent.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Represents the content of the engine configuration document
+ */
+export declare type EngineContent = {
+    /**
+     * Engine index
+     */
+    index: string;
+    /**
+     * Engine group
+     */
+    group: string;
+    /**
+     * Plugin name
+     */
+    name: string;
+};
